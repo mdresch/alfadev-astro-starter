a280e08e6fbef9025c5fec98153e9f4041c60a8d
Menno Drescher <menno@cbaconsult.eu>
2025-02-09 15:57:05 +0100

    Google Analytics Gtag

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JKFXL31B1D"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-JKFXL31B1D');
-</script>
